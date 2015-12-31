b53e90955d47cb2ad6ab96d96087ccab7ad77512
Johannes Muenzel <jmuenzel@gmail.com>
2015-12-30 23:56:36 -0800

    sketch nav

--- src/louds_trie/nav.rs
+++ src/louds_trie/nav.rs
@@ -5,7 +5,14 @@ struct Nav {
     state_: State
 }
 
-//impl Nav {
-//    fn has_child
-//}
+/*
+impl Nav {
+    fn has_child() -> bool;
+    fn next_sibling() -> bool;
+    fn up() -> bool;
+    fn is_terminal() -> bool;
+    fn get_string() -> &str;
+    fn is_end() -> bool
+}
+*/
 
