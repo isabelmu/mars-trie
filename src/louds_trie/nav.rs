6216d153e08cda7909a23b77c057367a1bb32d93
Johannes Muenzel <jmuenzel@gmail.com>
2016-01-01 14:17:49 -0800

    initial nav interface

--- src/louds_trie/nav.rs
+++ src/louds_trie/nav.rs
@@ -5,14 +5,33 @@ struct Nav {
     state_: State
 }
 
-/*
 impl Nav {
-    fn has_child() -> bool;
-    fn next_sibling() -> bool;
-    fn up() -> bool;
-    fn is_terminal() -> bool;
-    fn get_string() -> &str;
-    fn is_end() -> bool
+    fn has_child(&self) -> bool {
+        panic!("not implemented")
+    }
+    fn go_to_child(&mut self) -> bool {
+        panic!("not implemented")
+    }
+    fn has_sibling(&self) -> bool {
+        panic!("not implemented")
+    }
+    fn go_to_sibling(&mut self) -> bool {
+        panic!("not implemented")
+    }
+    fn has_parent(&self) -> bool {
+        panic!("not implemented")
+    }
+    fn go_to_parent(&self) -> bool {
+        panic!("not implemented")
+    }
+    fn is_terminal(&self) -> bool {
+        panic!("not implemented")
+    }
+    fn get_string(&self) -> &str {
+        panic!("not implemented")
+    }
+    fn is_end(&self) -> bool {
+        panic!("not implemented")
+    }
 }
-*/
 
